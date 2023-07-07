fb5fe31a1cbfe3c5a6eef40b13ab92f95610097f
Joonas Sandell <me@joonassandell.com>
2023-07-07 11:53:09 +0300

    Update loconative-scroll to work with the native scroll. Update lenis and related settings in loconative-scroll. (WIP)

--- lib/react-locomotive-scroll/LocomotiveScroll.context.jsx
+++ lib/react-locomotive-scroll/LocomotiveScroll.context.jsx
@@ -1,6 +1,4 @@
 import { createContext, useEffect, useRef, useState } from 'react';
-import { useDebounce } from 'use-debounce';
-import useResizeObserver from 'use-resize-observer';
 
 export const LocomotiveScrollContext = createContext({
   scroll: null,
@@ -10,40 +8,21 @@ export const LocomotiveScrollContext = createContext({
 export function LocomotiveScrollProvider({
   children,
   options,
-  containerRef,
   watch,
   onUpdate,
   location,
   onLocationChange,
 }) {
-  const { height: containerHeight } = useResizeObserver({ ref: containerRef });
   const [isReady, setIsReady] = useState(false);
   const LocomotiveScrollRef = useRef(null);
-  const [height] = useDebounce(containerHeight, 100);
-
-  if (!watch) {
-    console.warn(
-      'react-locomotive-scroll: you did not add any props to watch. Scroll may have weird behaviours if the instance is not updated when the route changes',
-    );
-  }
 
   useEffect(() => {
     (async () => {
       try {
-        const LocomotiveScroll = (await import('locomotive-scroll')).default;
-
-        const dataScrollContainer = document.querySelector(
-          '[data-scroll-container]',
-        );
-
-        if (!dataScrollContainer) {
-          console.warn(
-            `react-locomotive-scroll: [data-scroll-container] dataset was not found. You likely forgot to add it which will prevent Locomotive Scroll to work.`,
-          );
-        }
+        const LocomotiveScroll = // await import('./loconative-scroll')
+          (await import('./../loconative-scroll/loconative-scroll')).default;
 
         LocomotiveScrollRef.current = new LocomotiveScroll({
-          el: dataScrollContainer ?? undefined,
           ...options,
         });
 
@@ -59,20 +38,17 @@ export function LocomotiveScrollProvider({
     };
   }, []);
 
-  useEffect(
-    () => {
-      if (!LocomotiveScrollRef.current) {
-        return;
-      }
+  useEffect(() => {
+    if (!LocomotiveScrollRef.current) {
+      return;
+    }
 
-      LocomotiveScrollRef.current.update();
+    LocomotiveScrollRef.current.update();
 
-      if (onUpdate) {
-        onUpdate(LocomotiveScrollRef.current);
-      }
-    },
-    watch ? [...watch, height] : [height],
-  );
+    if (onUpdate) {
+      onUpdate(LocomotiveScrollRef.current);
+    }
+  }, [...watch]);
 
   useEffect(() => {
     if (!LocomotiveScrollRef.current || !location) {
